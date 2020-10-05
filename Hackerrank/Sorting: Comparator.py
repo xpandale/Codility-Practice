cdd776d872361e879deb2191785ea109b9d80d86
difuria <69213222+difuria@users.noreply.github.com>
2020-10-05 22:53:20 +0100

    Create Sorting: Comparator.py

---
+++ Hackerrank/Sorting: Comparator.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+from functools import cmp_to_key
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        
+    def __repr__(self):
+        pass
+
+    def comparator(a, b):
+        value = b.score - a.score
+        if value == 0:
+            if a.name > b.name:
+                value = 1
+            elif b.name > a.name:
+                value = -1
+            else:
+                value = 0
+        return value
+
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+    
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
